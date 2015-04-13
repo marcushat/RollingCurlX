b2ad348f38bc19489a7a092765f586354348dcbf
Julio Foulquié <julio@tnwlabs.com>
2015-04-13 14:00:55 +0200

    Update the test with a different API than Google's search.

--- tests/test.php
+++ tests/test.php
@@ -3,110 +3,28 @@
 
 /*
  * Adapted example from the code on https://github.com/petewarden/ParallelCurl
- *
  */
 
 
 require dirname(__DIR__) . '/vendor/autoload.php'; // Autoload files using Composer autoload
-//
-// A test script for the ParallelCurl class
-//
-// This example fetches a 100 different results from Google's search API, with no more
-// than 10 outstanding at any time.
-//
-// By Pete Warden <pete@petewarden.com>, freely reusable, see http://petewarden.typepad.com for more
+
+/**
+ * A test script for the RollingCurlX class
+ *
+ * This example fetches the value of €1 on different currencies through the freecurrencyconverterapi.com
+ *
+ * NOTE: freecurrencyconverterapi.com IS A FREE SERVICE, PLEASE, BE CONSIDERATE AND DON'T THROW TOO MANY REQUESTS
+ * AGAINST THEIR SERVERS.
+ *
+ * By Julio Foulquie <jfoulquie@gmail.com>, freely reusable.
+ */
 
 use marcushat\RollingCurlX;
 
-define ('SEARCH_URL_PREFIX', 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=large&filter=0');
+$base_url = 'http://www.freecurrencyconverterapi.com/api/v3/';
+$currencies = array();
 
-// This function gets called back for each request that completes
-function on_request_done($response, $url, $request_info, $user_data, $time) {
-
-	if ($request_info['http_code'] !== 200) {
-		print "Fetch error {$request_info['http_code']} for '$url'\n";
-		return;
-	}
-
-	$responseobject = json_decode($response, true);
-	if (empty($responseobject['responseData']['results'])) {
-		print "No results found for '$user_data'\n";
-		print_r($responseobject);
-		return;
-	}
-    if (empty($responseobject)) {
-		print "No results found for '$user_data'\n";
-        return;
-    }
-
-    print "********\n";
-	print "$user_data:\n";
-	print "********\n";
-
-	$allresponseresults = $responseobject['responseData']['results'];
-	foreach ($allresponseresults as $responseresult) {
-		$title = $responseresult['title'];
-		print "$title\n";
-	}
-
-
-}
-
-// The terms to search for on Google
-$terms_list = array(
-	"John", "Mary",
-	"William", "Anna",
-	"James", "Emma",
-	"George", "Elizabeth",
-	"Charles", "Margaret",
-	"Frank", "Minnie",
-	"Joseph", "Ida",
-	"Henry", "Bertha",
-	"Robert", "Clara",
-	"Thomas", "Alice",
-	"Edward", "Annie",
-	"Harry", "Florence",
-	"Walter", "Bessie",
-	"Arthur", "Grace",
-	"Fred", "Ethel",
-	"Albert", "Sarah",
-	"Samuel", "Ella",
-	"Clarence", "Martha",
-	"Louis", "Nellie",
-	"David", "Mabel",
-	"Joe", "Laura",
-	"Charlie", "Carrie",
-	"Richard", "Cora",
-	"Ernest", "Helen",
-	"Roy", "Maude",
-	"Will", "Lillian",
-	"Andrew", "Gertrude",
-	"Jesse", "Rose",
-	"Oscar", "Edna",
-	"Willie", "Pearl",
-	"Daniel", "Edith",
-	"Benjamin", "Jennie",
-	"Carl", "Hattie",
-	"Sam", "Mattie",
-	"Alfred", "Eva",
-	"Earl", "Julia",
-	"Peter", "Myrtle",
-	"Elmer", "Louise",
-	"Frederick", "Lillie",
-	"Howard", "Jessie",
-	"Lewis", "Frances",
-	"Ralph", "Catherine",
-	"Herbert", "Lula",
-	"Paul", "Lena",
-	"Lee", "Marie",
-	"Tom", "Ada",
-	"Herman", "Josephine",
-	"Martin", "Fanny",
-	"Jacob", "Lucy",
-	"Michael", "Dora",
-);
-
-if (isset($argv[1])) {
+if (isset($argv[1]) && is_numeric($argv[1])) {
     $max_requests = (int)$argv[1];
 } else {
     $max_requests = 10;
@@ -118,18 +36,76 @@ $curl_options = array(
     CURLOPT_USERAGENT, 'RollingCurlX test script',
 );
 
+echo "Using $max_requests concurrent requests at max." . PHP_EOL;
 $rolling_curl = new RollingCurlX($max_requests);
 $rolling_curl->setOptions($curl_options);
 
-foreach ($terms_list as $terms) {
-	$user_data = '"'.$terms.' is a"';
-	$search_url = SEARCH_URL_PREFIX.'&q='.urlencode($terms);
+
+// Retrieve currencies
+$currencies = $rolling_curl->addRequest($base_url . 'currencies', NULL, 'process_currencies');
+$rolling_curl->execute();
+
+// After running 'execute' the number of max_requests gets overwritten to the number of actual requests so reset it.
+///TODO: Fix this on the class, _maxConcurrent should go back to its original value after an 'execute' call.
+$rolling_curl->setMaxConcurrent($max_requests);
+$rolling_curl->setOptions($curl_options);
+
+foreach ($currencies as $currency => $data) {
+	$user_data = array("1€ in $currency", $currency);
+	$search_url = $base_url . 'convert?q=EUR_'.$currency. '&compact=y';
 	$rolling_curl->addRequest($search_url, NULL, 'on_request_done', $user_data);
 }
 
 $rolling_curl->execute();
 
-// This should be called when you need to wait for the requests to finish.
-// This will automatically run on destruct of the ParallelCurl object, so the next line is optional.
 
-?>
+/**
+ * CALLBACKS
+ */
+
+function process_currencies($response, $url, $request_info, $user_data, $time) {
+	global $currencies;
+
+	if ($request_info['http_code'] !== 200) {
+		print "Fetch error {$request_info['http_code']} for '$url'\n";
+		return;
+	}
+
+	$currencies = json_decode($response, true);
+	if (!is_array($currencies)) {
+		print "No results found for '{$user_data[1]}'\n";
+		print_r($responseobject);
+		return;
+	}
+
+	$currencies = isset($currencies['results']) ? $currencies['results'] : array();
+	echo "Total of " . count($currencies) . " currencies found." . PHP_EOL;
+}
+
+// This function gets called back for each request that completes
+function on_request_done($response, $url, $request_info, $user_data, $time) {
+
+	if ($request_info['http_code'] !== 200) {
+		print "Fetch error {$request_info['http_code']} for '$url'\n";
+		return;
+	}
+
+	$currency_key = 'EUR_' . $user_data[1];
+	$responseobject = json_decode($response, true);
+	if (empty($responseobject[$currency_key]['val'])) {
+		print "No results found for '{$user_data[1]}'\n";
+		return;
+	}
+
+	if (empty($responseobject)) {
+		print "No results found for '{$user_data[1]}'\n";
+		return;
+	}
+
+	$value = isset($responseobject[$currency_key]['val']) ? $responseobject[$currency_key]['val'] : 'NOT_FOUND' ;
+
+	print "********\n";
+	print "{$user_data[0]}: $value\n";
+	print "********\n";
+
+}
